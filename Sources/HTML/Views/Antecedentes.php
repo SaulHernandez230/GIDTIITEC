df640df3703dc352d950010dfdedb6aa0b2a3f87
saul hernandez <saulhernandez230@gmail.com>
2019-10-04 16:52:02 -0600

    header and footer components

---
+++ Sources/HTML/Views/Antecedentes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      require("../Components/header.html");
+      require("../Components/footer.html");
+    ?>
+
+
+  </body>
+</html>
