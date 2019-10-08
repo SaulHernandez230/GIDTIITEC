43369fe349ab8f52a4e27009a76146c2d5714103
saul hernandez <saulhernandez230@gmail.com>
2019-10-08 15:45:07 -0600

    begin responsive design

--- Public/index.php
+++ Public/index.php
@@ -1,28 +1,29 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>adsas</h1>
-    <?
-    $request = $_SERVER['REQUEST_URI'];
-    echo "$request";
-    switch ($request) {
-      case '/' :
-          require __DIR__ . '/views/index.php';
-          break;
-      case '' :
-          require __DIR__ . '/views/index.php';
-          break;
-      case '/Mison' :
-          require __DIR__ . '../Sources/HTML/Views/Mision.php';
-          break;
-      default:
-          require __DIR__ . '/views/404.php';
-          break;
-    }
-    ?>
-  </body>
-</html>
+<?php
+$request = explode("/",$_SERVER['REQUEST_URI']);
+require("../Sources/Components/Header.html");
+$url = "";
+if(isset($request[5]))
+  $url=$request[5];
+
+switch ($url){
+  case 'Antecedentes.php':
+    require("../Sources/Views/Antecedentes.php");
+    break;
+  case 'Mision.php':
+    require("../Sources/Views/Mision.php");
+    break;
+  case 'Proyectos.php':
+    require("../Sources/Views/Proyectos.php");
+    break;
+  case 'Premios.php':
+    require("../Sources/Views/Premios.php");
+    break;
+  case 'Contactanos.php':
+    require("../Sources/Views/Contactanos.php");
+    break;
+  default:
+      require_once __DIR__ . '/index.php';
+      break;
+}
+require("../Sources/Components/footer.html");
+?>
---
+++ Sources/Views/Proyectos.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Boostrap CSS Links -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- CSS Link -->
+    <link rel="stylesheet" href="../../Sources/CSS/proyectos.css">
+    <title></title>
+  </head>
+  <body>
+    <h2>Proyectos</h2>
+    <!--CAROUSEL-->
+    <div class="bd-example">
+      <div id="carouselExampleCaptions" class="carousel slide">
+        <ol class="carousel-indicators">
+          <li data-target="#options" data-slide-to="0" class="active"></li>
+          <li data-target="#options" data-slide-to="1"></li>
+          <li data-target="#options" data-slide-to="2"></li>
+          <li data-target="#options" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+          <!--Carousel Item-->
+          <div class="carousel-item active">
+            <!--Video-->
+            <iframe class="d-block w-100" height="315" src="https://www.youtube.com/embed/eqTUP8-kvek"  frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <!--Tittle Video-->
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Silla de ruedas motorizada</h5>
+            </div>
+          </div>
+          <!--Carousel Item-->
+          <div class="carousel-item">
+            <!--Video-->
+            <iframe class="d-block w-100" height="315"  src="https://www.youtube.com/embed/eu9tLt1OPGA" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <!--Tittle Video-->
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Proyecto "Hybri-Voz"</h5>
+            </div>
+          </div>
+          <!--Carousel Item-->
+          <div class="carousel-item">
+            <!--Video-->
+            <iframe class="d-block w-100" height="315" src="https://www.youtube.com/embed/o2q7CoxniYA" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <!--Tittle Video-->
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Primero Noticias</h5>
+            </div>
+          </div>
+          <!--Carousel Item-->
+          <div class="carousel-item">
+            <!--Video-->
+            <iframe class="d-block w-100" height="315"  src="https://www.youtube.com/embed/hs9QHfi419c" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <!--Tittle Video-->
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Con_Ciencia Pi</h5>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+    <!--Proyects information-->
+    <!--proyect 1-->
+    <div class="proyect-container">
+      <img src="../../Sources/Img/gidtiitec.png">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <!--proyect 2-->
+    <div class="proyect-container">
+      <img src="../../Sources/Img/gidtiitec.png">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <!--proyect 3-->
+    <div class="proyect-container">
+      <img src="../../Sources/Img/gidtiitec.png">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <!--proyect 4-->
+    <div class="proyect-container">
+      <img src="../../Sources/Img/gidtiitec.png">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <!-- Boostrap Scripts-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
