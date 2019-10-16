86383d2843996c4cc1697c1716d45917479cbe29
saul hernandez <saulhernandez@iMac-de-saul.local>
2019-10-15 21:08:17 -0600

    mision responsive design

--- Sources/Views/Antecedentes.php
+++ Sources/Views/Antecedentes.php
@@ -7,20 +7,24 @@
        aplicado a diversos sectores y comunidades que reflejan el espíritu solidario de quienes componen este gran grupo.
        Estos grandes proyectos se ilustran a partir de proyectos aplicados, prototipos, artículos, capítulos de libros,
        congresos, charlas, exposiciones, foros, así como las investigaciones desarrolladas que han permitido contar con
-       la producción referida. La cuantificación y la proyección del trabajo realizado son un buen ejemplo de las
-       posibilidades que se gestan en el Grupo en el marco de la Universidad la Universidad Autónoma de Baja California
-       Sur.Es por esta razón que los investigadores actuales del Grupo busca realizar investigaciones de calidad cuyos
-       resultados pretenden ser productivos para la sociedad nacional y regional.
+       la producción referida.
     </p>
     <p>
       Profesores visionarios de los Departamentos Académicos de Sistemas Computacionales y Humanidades, crearon este
-      Grupo para impulsar la investigación inclusiva y educativa, la propuesta se inspiró por este deseo. En cuanto a la 
+      Grupo para impulsar la investigación inclusiva y educativa, la propuesta se inspiró por este deseo. En cuanto a la
       trascendencia regional, el Grupo se convierte en una valiosa fuente de asesoramiento en el campo de desarrollo de
       tecnologías inclusivas e innovación educativa, no solo para la Secretaria de Educación Pública, quien se nutriría
       de los resultados de estudios para la toma de decisiones, sino de los diferentes sectores públicos y privados que
-       buscan impulsar el bienestar social y económico a través de la inclusión y el acortamiento de la brecha educativa
-       aprovechando las tecnologías propuestas.
+      buscan impulsar el bienestar social y económico a través de la inclusión y el acortamiento de la brecha educativa
+      aprovechando las tecnologías propuestas.
+    </p>
+    <p>
+      La cuantificación y la proyección del trabajo realizado son un buen ejemplo de las
+      posibilidades que se gestan en el Grupo en el marco de la Universidad la Universidad Autónoma de Baja California
+      Sur.Es por esta razón que los investigadores actuales del Grupo busca realizar investigaciones de calidad cuyos
+      resultados pretenden ser productivos para la sociedad nacional y regional.
     </p>
   </div>
-  <img id="timeLine" src="../../Sources/Img/PROYECTOS DASC.png">
+  <span class="sr-only" >Exposiencias Nacional 2016 </span>
+  <img id="timeLine" src="../../Sources/Img/PROYECTOS DASC.png" aria-label="Participacion del Grupo de desarrollo en Exposiencias ">
 </body>
--- Sources/Views/Mision.php
+++ Sources/Views/Mision.php
@@ -1,56 +1,64 @@
 <body>
   <div id="Mision">
     <!-- <h2>Mision, Vision y Objetivos</h2> -->
-    <h3>Mision</h3>
-    <p>
-      Somos un equipo multidisciplinario dedicado a contribuir y fomentar la investigación, análisis, e innovación,
-      así como la difusión de la cultura tecnológica y educativa, desarrollando y aplicando tecnología inclusiva y
-      educativa favoreciendo el progreso y la mejora de la calidad de vida de comunidades a través de las Tecnologías
-      de la Información y Comunicación.
-    </p>
-    <h3>Vision</h3>
-    <p>
-      Seremos un grupo de investigación referente y de vanguardia en investigación, innovación y desarrollo de
-      proyectos inclusivos e innovación educativa permitiendo reducir la brecha tecnológica en los sectores mas
-      vulnerables.
-    </p>
-    <h3>Objetivos</h3>
-    <p>
-      Generar y aplicar nuevos conocimientos para el desarrollo tecnológico y la innovación en los campos de
-      inclusión y educación, mediante el uso de nuevas tecnologías y herramientas.
-    </p>
-    <h3>Objetivos Especificos</h3>
-    <ul role="list">
+    <div class="M">
+      <h3>Mision</h3>
+      <p aria-label=" Mision de GIDTIITEC ">
+        Somos un equipo multidisciplinario dedicado a contribuir y fomentar la investigación, análisis, e innovación,
+        así como la difusión de la cultura tecnológica y educativa, desarrollando y aplicando tecnología inclusiva y
+        educativa favoreciendo el progreso y la mejora de la calidad de vida de comunidades a través de las Tecnologías
+        de la Información y Comunicación.
+      </p>
+    </div>
+    <div class="VO">
+      <h3>Vision</h3>
+      <p aria-label=" Vision de GIDTIITEC ">
+        Seremos un grupo de investigación referente y de vanguardia en investigación, innovación y desarrollo de
+        proyectos inclusivos e innovación educativa permitiendo reducir la brecha tecnológica en los sectores mas
+        vulnerables.
+      </p>
+    </div>
+    <div class="VO">
+      <h3>Objetivos</h3>
+      <p aria-label=" Objetivos de GIDTIITEC ">
+        Generar y aplicar nuevos conocimientos para el desarrollo tecnológico y la innovación en los campos de
+        inclusión y educación, mediante el uso de nuevas tecnologías y herramientas.
+      </p>
+    </div>
+    <div class="OE">
+      <h3>Objetivos Especificos</h3>
       <span class="sr-only" >Los objetivos específicos de GIDTIITEC son:</span>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 1">
-        Mejorar la calidad de vida en los grupos vulnerables,a través de la generación de tecnologías inclusivas y
-        educativas.
-      </li>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 2"> Ayudar a tener mejor calidad de vida a los grupos vulnerables.</li>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 3">
-        Potenciar la capacidad académica de los estudiantes de los diferentes niveles educativos.
-      </li>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 4">
-        Responder a las necesidades y expectativas pertinentes de los usuarios finales.
-      </li>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 5">
-        Brindar asesorías en investigación, desarrollo y aplicación de tecnologías inclusivas y educativas
-      </li>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 6">
-        Brindar asesorías en la elaboración y desarrollo de tesis de licenciatura y posgrado.
-      </li>
-      <!--objetivo especifico-->
-      <li role="listitem" aria-label="objetivo 6">
-        Consolidar el Laboratorio de Desarrollo de Tecnologías Inclusivas e Innovación Educativa de la
-        Universidad Autónoma de Baja California Sur.
-      </li>
-    </ul
-    <img src="" alt="">
+
+      <ul role="list" aria-label="Lista de objetivos Especificos">
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 1">
+          Mejorar la calidad de vida en los grupos vulnerables,a través de la generación de tecnologías inclusivas y
+          educativas.
+        </li>
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 2"> Ayudar a tener mejor calidad de vida a los grupos vulnerables.</li>
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 3">
+          Potenciar la capacidad académica de los estudiantes de los diferentes niveles educativos.
+        </li>
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 4">
+          Responder a las necesidades y expectativas pertinentes de los usuarios finales.
+        </li>
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 5">
+          Brindar asesorías en investigación, desarrollo y aplicación de tecnologías inclusivas y educativas
+        </li>
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 6">
+          Brindar asesorías en la elaboración y desarrollo de tesis de licenciatura y posgrado.
+        </li>
+        <!--objetivo especifico-->
+        <li role="listitem" aria-label="objetivo 7">
+          Consolidar el Laboratorio de Desarrollo de Tecnologías Inclusivas e Innovación Educativa de la
+          Universidad Autónoma de Baja California Sur.
+        </li>
+      </ul
+    </div>
   </div>
 </body>
