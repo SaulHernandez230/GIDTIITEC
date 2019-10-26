e2764f19c6c3d68364158cbc9e60efc517b26971
Erick <erick_sbas@hotmail.com>
2019-10-26 14:57:32 -0600

    Se empezo la vista de premios y reconocimientos

--- Sources/Views/Rewards.php
+++ Sources/Views/Rewards.php
@@ -1,5 +1,144 @@
 <body>
-  <div id="Premios">
-    <h2>Premios</h2>
+  <div id="Premios" class="container">
+    <div class="titulo">
+      <h2 class="my-5">Premios y reconocimientos</h2>
+    </div>
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div class="card my-3">
+          <div class=" card-body">
+            <h5 class="card-title">Titulo/Icono</h5>
+            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <ul class="list-unstyled">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </body>
