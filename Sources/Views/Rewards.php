7f60ca9030ea0db06a38f7d14fea9fafc4b203de
Erick <erick_sbas@hotmail.com>
2019-10-27 10:45:49 -0700

    Asi mero alv[Rewards]

--- Sources/Views/Rewards.php
+++ Sources/Views/Rewards.php
@@ -4,137 +4,136 @@
       <h2 class="my-5">Premios y reconocimientos</h2>
     </div>
     <div class="row">
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/medalSilver.svg" alt="Medalla plata"></h5>
+            <p class="card-text">21° Semana Nacional de Ciencia y Tecnología.</p><br><br>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con-Ciencia. </li><br>
+              <li><b>Fecha: </b>15 de octubre de 2014.</li><br>
+              <li><b>Categoría: </b>Nivel superior.</li><br>
+              <li><b>Reconocimiento: </b>2do Lugar.</li><br><br>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/medalGold.svg" alt="Medalla plata"></h5>
+            <p class="card-text">22ª semana nacional de ciencia y tecnología.</p><br><br>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con_Ciencia 2.1</li><br>
+              <li><b>Fecha: </b>22 de octubre de 2015.</li><br>
+              <li><b>Categoría: </b>Nivel superior.</li><br>
+              <li><b>Reconocimiento: </b>1er Lugar.</li><br><br>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/medalGold.svg" alt="Medalla plata"></h5>
+            <p class="card-text">Expo Ciencia Sudcaliforniana 2016 (Etapa Estatal).</p>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con_Ciencia Pi.</li>
+              <li><b>Area: </b>Ciencias de la ingeniería.</li>
+              <li><b>Fecha: </b>9 y 10 de junio de 2016.</li>
+              <li><b>Categoría: </b>Nivel superior.</li>
+              <li><b>Reconocimiento: </b>
+                1er Lugar a nivel superior, 1er categoría de Ciencias de la Ingeniería, 1er Lugar en el Área de
+                Ingeniería y se obtuvo la acreditación para pertenecer a la delegación sudcaliforniana en la Expo
+                Ciencias Nacional en la sede Villahermosa, Tabasco.</li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/medalSilver.svg" alt="Medalla plata"></h5>
+            <p class="card-text">23ª semana nacional de ciencia y tecnología.</p><br>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con_Ciencia Pi.</li><br>
+              <li><b>Fecha: </b>3 de diciembre de 2016.</li><br>
+              <li><b>Categoría: </b>Nivel superior.</li><br>
+              <li><b>Reconocimiento: </b>2do Lugar.</li><br>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/medalSilver.svg" alt="Medalla plata"></h5>
+            <p class="card-text">Expo Ciencias Nacional Villahermosa, Tabasco 2016.</p>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con_Ciencia Pi. </li>
+              <li><b>Area: </b>Ciencias de la ingeniería.</li>
+              <li><b>Fecha: </b> 7 al 10 de diciembre de 2016.</li>
+              <li><b>Categoría: </b> Nivel superior.</li>
+              <li><b>Reconocimiento: </b>Se obtuvo la acreditación para representar a México en la Expo Ciencias
+                Internacional en la sede de Bruselas, Bélgica.</li><br>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/document.svg" alt="Medalla plata"></h5>
+            <p class="card-text">Expo Ciencias Internacional Bruselas, Bélgica 2017.</p>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <br><li><b>Proyecto: </b>Con_Ciencia Pi.</li><br>
+              <li><b>Area: </b>Ciencias de la ingeniería.</li><br>
+              <li><b>Fecha: </b>Abril 2017.</li><br>
+              <li><b>Reconocimiento: </b>Constancia de participación.</li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/document.svg" alt="Medalla plata"></h5>
+            <p class="card-text">Ciencia en Acción (1ra Etapa)</p>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con_Ciencia Pi.</li><br>
+              <li><b>Fecha: </b>Marzo de 2017 a Mayo de 2017 (Etapa de selección)</li><br>
+              <li><b>Categoría:</b> Ciencia, Ingeniería y Valores.</li><br>
+              <li><b>Reconocimiento: </b>Se obtuvo la acreditación para representar a México en la sede de España en las
+                ciudades de Ermua y Eibar.</li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
+            <h5 class="card-title"><img src="../../Sources/Img/document.svg" alt="Reconocimiento"></h5>
+            <p class="card-text">Ciencia en Acción (2da Etapa)</p>
             <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+              <li><b>Proyecto: </b>Con_Ciencia Pi.</li>
+              <li><b>Fecha: </b>6 al 8 de octubre se celebró el concurso en la etapa final (demostración del prototipo).</li>
+              <li><b>Reconocimiento: </b>Por configurar un diseño y, al amparo del mismo, llegar a construir todo un artefacto, para que se
+                respeten más los derechos de las personas con necesidades especiales, se concede Mención de Honor al
+                trabajo "Con Ciencia PI", reconocimiento a nivel Internacional.</li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card my-3">
           <div class=" card-body">
-            <h5 class="card-title">Titulo/Icono</h5>
-            <p class="card-text">Nose si quieres un texto aqui o que pedo pero aqui te dejo esto jeje</p>
-            <ul class="list-unstyled">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
+            <h5 class="card-title"><img src="../../Sources/Img/medalGold.svg" alt="Medalla oro"></h5>
+            <p class="card-text">Feria Nacional de Ciencias e Ingenierías FENACI 2017.</p><br>
+            <ul class="list-unstyled ">
+              <li><b>Proyecto: </b>Sam Speech</li><br>
+              <li><b>Categoría: </b>Ingeniería y Computación</li><br>
+              <li><b>Fecha: </b>Noviembre 2017</li><br>
+              <li><b>Reconocimiento: </b>1er. lugar</li><br><br>
             </ul>
           </div>
         </div>
