b60907b90af264d127a173826a1e8878e3689c92
saul hernandez <saulhernandez230@gmail.com>
2019-11-17 18:36:39 -0700

    sin poder lograrlo

--- Sources/JS/switchAnchor.js
+++ Sources/JS/switchAnchor.js
@@ -1,23 +1,25 @@
 //activar y desactivar anchor
 $(document).ready(function(){
-  
-  $("a").click(function(){
-    var currentPageId = this.id
-    var currentPage = this.textContent
-    var bCurrentPageId = "b"+this.id
-    speechSynthesis.cancel()
-
-    sessionStorage.setItem("saveClass","nav-link active")
-    sessionStorage.setItem("anchorId",currentPageId)
-    sessionStorage.setItem("breadId",bCurrentPageId)
-
-    window.location = "/GIDTIITEC/Public/index.php/"+currentPage + ".php";
-  });
-
   var anchor = document.getElementById(sessionStorage.getItem("anchorId"))
-  var bread = document.getElementById(sessionStorage.getItem("breadId"))
-  anchor.className =sessionStorage.getItem("saveClass")
-  bread.style ="display:initial"
-  var fn = window["highContrast"]
-    fn()
+    var bread = document.getElementById(sessionStorage.getItem("breadId"))
+    anchor.className =sessionStorage.getItem("saveClass")
+    bread.style ="display:initial"
+    var fn = window["highContrast"]
+      fn()
+  
 });
+$("a").click(function(){
+  var currentPageId = this.id
+  var currentPage = this.textContent
+  var bCurrentPageId = "b"+this.id
+  speechSynthesis.cancel()
+
+  sessionStorage.setItem("saveClass","nav-link active")
+  sessionStorage.setItem("anchorId",currentPageId)
+  sessionStorage.setItem("breadId",bCurrentPageId)
+
+  window.location = "/GIDTIITEC/Public/index.php/"+currentPage + ".php";
+});
+
+window.onload = () =>{ 
+};
